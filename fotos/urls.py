fa8c3376fa8e190f9d423da0c4144a877651b713
rfamiglietti <romulopfami@gmail.com>
2025-08-28 13:56:59 -0300

    alteração fotos/views.py e criacão de fotos/urls.py

---
+++ fotos/urls.py
@@ -0,0 +1,9 @@
+# fotos/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('imagem/<int:foto_id>', views.imagem, name='imagem'),
+]
--- fotos/views.py
+++ fotos/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Fotografia
 
 # Create your views here.
+
+def index(request):
+    fotografias = Fotografia.objects.order_by("data_foto").filter(publicada=True)
+    return render(request, 'fotos/index.html', {'cards': fotografias})
+
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'fotos/imagem.html', {"fotografia": fotografia})
