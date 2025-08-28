d903ddc5a3c9077bc0b401bf8cce8c2cdf1cbc5d
rfamiglietti <romulopfami@gmail.com>
2025-08-28 20:44:09 -0300

    função pesquisar

--- fotos/urls.py
+++ fotos/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('imagem/<int:foto_id>', views.imagem, name='imagem'),
+    path('pesquisar', views.pesquisar, name='pesquisar'),
 ]
--- fotos/views.py
+++ fotos/views.py
@@ -1,7 +1,8 @@
+# fotos/views.py
+
 from django.shortcuts import render, get_object_or_404
 from .models import Fotografia
-
-# Create your views here.
+from django.db.models import Q
 
 def index(request):
     fotografias = Fotografia.objects.order_by("data_foto").filter(publicada=True)
@@ -9,4 +10,24 @@ def index(request):
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
-    return render(request, 'fotos/imagem.html', {"fotografia": fotografia})
+    return render(request, 'fotos/imagem.html', {'fotografia': fotografia})
+
+def pesquisar(request):
+    fotografias = Fotografia.objects.order_by("data_foto").filter(publicada=True)
+    
+    termo_de_busca = request.GET.get('buscar')
+    
+    if termo_de_busca:
+        fotografias = fotografias.filter(
+            Q(nome__icontains=termo_de_busca) | 
+            Q(legenda__icontains=termo_de_busca) | 
+            Q(categoria__icontains=termo_de_busca)
+        ).order_by("data_foto")
+            
+    # Adicione o termo de busca ao contexto
+    contexto = {
+        "cards": fotografias,
+        "termo_de_busca": termo_de_busca
+    }
+            
+    return render(request, 'fotos/pesquisa.html', contexto)
